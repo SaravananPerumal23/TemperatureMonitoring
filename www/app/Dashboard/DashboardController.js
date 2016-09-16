132d6efb5c06dd59e15c74d4eb57f6c2a618ada1
Saravanan.Perumal <Saravanan.Perumal@target.com>
2016-09-15 21:43:08 -0500

    Updated with API call for Charts

--- www/app/Dashboard/DashboardController.js
+++ www/app/Dashboard/DashboardController.js
@@ -2,17 +2,45 @@ angular.module('ionicCharts')
 
 .controller("DashboardController", function($scope, $http, $state, AuthService) {
 
-  $scope.url = "https://api.ipify.org/";
-      $http({
-          method: 'jsonp',
-          url: $scope.url,
-          params: {
-              format: 'jsonp',
-              callback: 'JSON_CALLBACK'
-          }
-      }).then(function (resp) {
-          $scope.responseData = resp.data.ip;
-      });
+  // $scope.url = "http://localhost:8888/api/AccountUnitItems/read/";
+  //     $http({
+  //         method: 'jsonp',
+  //         url: $scope.url,
+  //         params: {
+  //             format: 'jsonp',
+  //             callback: 'JSON_CALLBACK'
+  //         }
+  //     }).then(function (resp) {
+  //         $scope.responseData = resp.data.UnitItem.UnitID;
+  //     });
+
+  $http.get('http://localhost:8888/api/AccountUnitItems/read/')
+  .then(function(unitResponse){
+  		console.log('Success', unitResponse); // JSON object
+      $scope.unitReponseData = unitResponse.data.UnitItem;
+  	}, function(err){
+  		console.error('ERR', err);
+  	})
+
+    $http.get('http://localhost:8888/api/AccountSPointItems/read')
+    .then(function(sensorPointResponse){
+        console.log('Success', sensorPointResponse); // JSON object
+        $scope.sensorPointResponseData = sensorPointResponse.data;
+      }, function(err){
+        console.error('ERR', err);
+      })
+
+  // $scope.url = "https://api.ipify.org/";
+  //     $http({
+  //         method: 'jsonp',
+  //         url: $scope.url,
+  //         params: {
+  //             format: 'jsonp',
+  //             callback: 'JSON_CALLBACK'
+  //         }
+  //     }).then(function (resp) {
+  //         $scope.responseData = resp.data.ip;
+  //     });
 
 
   // $http.get('https://api.ipify.org?format=json', params: {
@@ -31,10 +59,19 @@ angular.module('ionicCharts')
     $state.go('login');
   };
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-  ];
+  // $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  // $scope.series = ['Series A', 'Series B'];
+  // $scope.data = [
+  //     [65, 59, 80, 81, 56, 55, 40],
+  //     [28, 48, 40, 19, 86, 27, 90]
+  // ];
+
+
+
+  // $scope.labels = ["DeviceID", "February", "March", "April", "May", "June", "July"];
+  // $scope.series = ['Series A', 'Series B'];
+  // $scope.data = [
+  //     [65, 59, 80, 81, 56, 55, 40],
+  //     [28, 48, 40, 19, 86, 27, 90]
+  // ];
 });
